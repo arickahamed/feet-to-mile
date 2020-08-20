2ac330c290d1e96b1c004af0fcfcdf5f73cf3c4a
Arick Ahamed <arickahamed700@gmail.com>
2020-08-20 22:14:15 +0600

    first commit

---
+++ feetToMile.js
@@ -0,0 +1,14 @@
+function feetToMile(measuerment) {
+    if (measuerment < 0) {
+        alert("Your Input Is Invalid!");
+       return calculation;
+    }else{
+        const calculation = (measuerment * 0.00019);
+        return calculation;
+    }
+    // const calculation = (measurement * 0.00019);
+    // return calculation;
+}
+const result = feetToMile(-500);
+// const result = Math.round(input);
+console.log( result);
